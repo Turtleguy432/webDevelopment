3fa11f2b3317058448ef907193abfa5276c54ce2
Ashton Carlson <turtleguy432@gmail.com>
2022-11-18 15:55:21 -0800

    Added Functions, and improved Color Change func

--- JSS/Sort.c
+++ JSS/Sort.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 
-int arrayReturn();
-int sortArray();
+void sortArray(int [], int);
 
 int main(int argc, char* argv[])
 {
-	int unsortedArray = arrayReturn();
-	for (int i = 0; i < sizeof(unsortedArray); ++i)
-	{
-		printf("%d\n", unsortedArray[i] );
-	
+	int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	for (int i = 0; i < 11; ++i) {
+		printf("%d ", array[i]);
+	}
+	printf("%s\n", "Before sort");
+	sortArray(array, 11);
+	for (int i = 0; i < 11; ++i) {
+		printf("%d ", array[i]);
 	}
 
 	return 0;
 }
 
-int arrayReturn()
-{
-	randomNum=rand()
-	int array[] = "";
-	sizeof(array) = getchar(); 
 
+
+
+
+
+void sortArray(int array[], int arraySize) {
+	
+	for (int i = 1; i < arraySize; ++i)
+	{
+		int l = i - 1;
+		int temp = array[i];
+		while (l >= 0 && array[l] > temp) {
+			array[l + 1] = array[l];
+		l--;
+		}
+
+		array[l + 1] = temp;
+	}
 
 }
+
+
